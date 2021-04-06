5428c5b99e5378d28afd498b045e373816740447
AndyMc629 <andrewpmcmahon629@gmail.com>
2021-04-06 13:55:48 +0100

    Fixing pipeline for spark mllib chapter 3

--- chapter3/pipelines/sparkmllib_pipeline.py
+++ chapter3/pipelines/sparkmllib_pipeline.py
@@ -1,103 +1,73 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
 
 from pyspark.sql import functions as f
 from pyspark.mllib.evaluation import BinaryClassificationMetrics, MulticlassMetrics
-from pyspark.ml.feature import StandardScaler
+from pyspark.ml.feature import StandardScaler, OneHotEncoder, StringIndexer, Imputer, VectorAssembler
 
 from pyspark.ml import Pipeline, PipelineModel
-
-categoricalColumns = [“col1”, “col2”, …]
-
-stages = []
-
-for categoricalCol in categoricalColumns:
-    # fill some nulls
-    #data = data.na.fill({categoricalCol:’Unknown’})
-    # category indexing with string indexer
-    # stringIndexer = StringIndexer(inputCol=categoricalCol, outputCol=categoricalCol+”Index”).setHandleInvalid(“keep”) # keep is for unknown categories
-    # Use onehotencoder to convert cat variables into binary sparseVectors
-    encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol+”classVec”])
-    # Add stages. These are not run here, will be run later
-    stages += [stringIndexer, encoder]
-
-numericCols = [“numCol1”]
-assemblerInputs = [c+”classVec” for c in categoricalColumns]+numericCols
-Assembler = VectorAssembler(inputCols=assemblerInputs, outputCol=”features”) # features contains everything, not hot encoded and numerical
-stages += [assembler]
-
-# ML MODEL
 from pyspark.ml.classification import LogisticRegression
 
-data_transformation_pipeline = Pipeline().setStages(stages).fit(data)
-prepped_data = data_transformation_pipeline.transform(data)
+if __name__ == "__main__":
+    # Create spark context
+    sc = SparkContext("local", "pipelines")
+    # Get spark session
+    spark = SparkSession.builder.getOrCreate()
 
-# Random train test split with seed
-(trainingData, testData) = prepped_data.randomSplit([0.7,0.3], seed=100)
+    # Get the data and place it in a spark dataframe
+    data = spark.read.format("csv").option("sep", ";").option("inferSchema", "true").option("header", "true").load(
+        "../../chapter1/stream-classifier/data/bank/bank.csv")
 
-# model
-lr = LogisticRegression(labelCol=”label”, featuresCol=”features”, maxIter=10)
+    # map target to numerical category
+    data = data.withColumn('label', f.when((f.col("y") == "yes"), 1).otherwise(0))
 
-lrModel = lr.fit(trainingData)
+    # define list for storage stage references
+    stages = []
 
-# evaluate
-from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
+    # define list for storage stage references
+    stages = []
 
-evaluator = BinaryClassificationEvaluator(rawPredictionCol=”rawPrediction”)
+    # define the transformation stages for the categorical columns
+    categoricalColumns = ["job", "marital", "education", "contact", "housing", "loan", "default", "day"]
+    for categoricalCol in categoricalColumns:
+        # fill some nulls
+        # data = data.na.fill({categoricalCol:’Unknown’})
+        # category indexing with string indexer
+        stringIndexer = StringIndexer(inputCol=categoricalCol,
+                                      outputCol=categoricalCol + "Index").setHandleInvalid(
+            "keep")  # keep is for unknown categories
+        # Use onehotencoder to convert cat variables into binary sparseVectors
+        encoder = OneHotEncoder(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
+        # Add stages. These are not run here, will be run later
+        stages += [stringIndexer, encoder]
 
-paramGrid = (ParamGridBuilder()
-                .addGrid(lr.regParam, [0.01, 0.5, 2.0])
-                .addGrid(lr.elasticNetParam, [0.0, 0.5, 1.0])
-                .addGrid(lr.maxIter, [1, 5, 10])
-                .build()
-estimatorPipeline = Pipeline(stages=[lr])
+    # define impute stage for the numerical columns
+    numericalColumns = ["age", "balance"]
+    numericalColumnsImputed = [x + "_imputed" for x in numericalColumns]
+    imputer = Imputer(inputCols=numericalColumns, outputCols=numericalColumnsImputed)
+    stages += [imputer]
 
-cv = CrossValidator(
-    estimator = estimatorPipeline,
-    estimatorParamMaps = paramGrid,
-    evaluator = evaluator,
-    numFolds=5
-)
+    # define numerical assembler first for scaling
+    numericalAssembler = VectorAssembler(inputCols=numericalColumnsImputed, outputCol='numerical_cols_imputed')
+    stages += [numericalAssembler]
 
-cvModel = cv.fit(trainingData)
+    # define the standard scaler stage for the numerical columns
+    scaler = StandardScaler(inputCol='numerical_cols_imputed', outputCol="numerical_cols_imputed_scaled")
+    stages += [scaler] # already a list so no need for brackets
 
+    # Perform assembly stage to bring together features
+    assemblerInputs = [c + "classVec" for c in categoricalColumns] + ["numerical_cols_imputed_scaled"]
+    # features contains everything, one hot encoded and numerical
+    assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+    stages += [assembler]
 
-# MODEL EVALUATION
-def evaluate(predictionAndLabels):
-    log = {}
-    # show validation score (AUROC)
-    evaluator = BinaryClassificationEvaluator(metricName=’areaUnderROC’)
-    log[‘AUROC’] = “%f” % evaluator.evaluate(predictionAndLabels)
-    print(“Area under ROC = {}”.format(log[‘AUROC’])
+    # define the model stage at the end of the pipeline
+    lr = LogisticRegression(labelCol="label", featuresCol="features", maxIter=10)
+    stages += [lr]
 
-    # show validation score (AUPR)
-    evaluator = BinaryClassificationEvaluator(metricName=”areaUnderPR”)
-    log[“AUPR”] = “%f” % evaluator.evaluate(predictionAndLabels)
-    print(“Area under PR = {}”.format(log[“AUPR”]))
+    # Random train test split with seed
+    (trainingData, testData) = data.randomSplit([0.7, 0.3], seed=100)
 
-    # Metrics
-    predictionRDD = predictionAndLabels.select([‘label’,’prediction’]).rdd.map(lambda line: (line[1], line[0]))
-    Metrics = MulticlassMetrics(predictionRDD)
-
-    print(metrics.confusionMatrix().toArray())
-
-    # Overall statistics
-    log[‘precision’] = “%s” % metrics.precision()
-    log[‘recall’] = “%s” % metrics.recall()
-    log[‘F1_measure’] = “%s” % metrics.fMeasure()
-
-    # Statistics by class
-    labels =  [0.0, 1.0]
-    for label in sorted(labels):
-        log[label] = {}
-        log[label][‘precision’] = “%s” % metrics.precision(label)
-        log[label][‘recall’] = “%s” % metrics.recall()
-        log[label][‘F1_measure’] = “%s” % metrics.fMeasure(label, beta=1.0)
-
-return log
-
-
-
-
-
-
-
+    # Define the entire pipeline and fit on the train data and transform on the test data
+    clfPipeline = Pipeline().setStages(stages).fit(trainingData)
+    clfPipeline.transform(testData)
