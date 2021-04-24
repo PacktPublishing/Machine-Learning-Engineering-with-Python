184486c53f9e55c36ae0f8fbab2526a4e2bd9174
AndyMc629 <andrewpmcmahon629@gmail.com>
2021-04-24 14:25:08 +0100

    Package and other examples for chapter 4

---
+++ chapter4/outlier_package/outliers/detectors.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import IsolationForest
+from sklearn.datasets import make_blobs
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+
+class OutlierDetector(object):
+    def __init__(self, model=None):
+        if model is not None:
+            self.model = model
+
+        self.pipeline = make_pipeline(StandardScaler(), self.model)
+
+    def detect(self, data):
+        return self.pipeline.fit(data).predict(data)
