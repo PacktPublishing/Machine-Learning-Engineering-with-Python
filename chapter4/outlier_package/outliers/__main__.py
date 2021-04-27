ba65ae424174000a4739e4b7714d49b07f2b0cca
AndyMc629 <andrewpmcmahon629@gmail.com>
2021-04-27 10:36:35 +0100

    Changed how configs are stored and test definitions

--- chapter4/outlier_package/outliers/__main__.py
+++ chapter4/outlier_package/outliers/__main__.py
@@ -1,16 +1,13 @@
 from utils.data import create_data
 from detectors.detection_models import DetectionModels
-import detectors._base
+import detectors.pipelines
+from definitions import MODEL_CONFIG_PATH
 
-import os
-dirname = os.path.dirname(__file__)
-MODEL_CONFIG_PATH = os.path.join(dirname, "configs/model_config.json")
-#MODEL_CONFIG_PATH = "./configs/model_config.json"
 
 if __name__ == "__main__":
     data = create_data()
     models = DetectionModels(MODEL_CONFIG_PATH).get_models()
     for model in models:
-        detector = detectors._base.OutlierDetector(model=model)
+        detector = detectors.pipelines.OutlierDetector(model=model)
         result = detector.detect(data)
         print(result)
