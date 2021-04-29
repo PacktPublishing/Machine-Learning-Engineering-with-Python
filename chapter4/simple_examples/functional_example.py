dd2a3cba7542ec5bfdf4749f571c05cedb8eae61
AndyMc629 <andrewpmcmahon629@gmail.com>
2021-04-29 19:37:39 +0100

    Fucntional programming example

--- chapter4/simple_examples/functional_example.py
+++ chapter4/simple_examples/functional_example.py
@@ -1,4 +1,14 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({'data': np.linspace(0,1,100)})
+data = [
+    ['The', 'Business', 'Centre', '15', 'Stevenson', 'Lane'],
+    ['6', 'Mossvale', 'Road'],
+    ['Studio', '7', 'Tottenham', 'Court', 'Road']
+]
+
+def len_strings_in_list(data_list):
+    return list(map(lambda x: len(x), data_list))
+
+def list_of_list_func_results(list_func, list_of_lists):
+    return list(map(lambda x: list_func(x), list_of_lists))
