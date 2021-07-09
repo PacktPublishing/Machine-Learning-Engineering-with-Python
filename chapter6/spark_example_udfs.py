78b7bad19362be337757eed0ab050d6cad227b77
AndyMc629 <andrewpmcmahon629@gmail.com>
2021-07-09 13:04:20 +0100

    Spark basic example for chapter 6 udf - month conversion 1

--- chapter6/spark_example_udfs.py
+++ chapter6/spark_example_udfs.py
@@ -1,6 +1,7 @@
 #Note - reuses some code from chapter 3
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
+from pyspark.sql.types import StringType
 from pyspark.sql import functions as f
 
 
@@ -14,6 +15,18 @@ data = spark.read.format("csv").option("sep", ";").option("inferSchema", "true")
     "../chapter1/stream-classifier/data/bank/bank.csv")
 
 # map target to numerical category
-data = data.withColumn('label', f.when((f.col("y") == "yes"), 1).otherwise(0))
+#data = data.withColumn('label', f.when((f.col("y") == "yes"), 1).otherwise(0))
+
+data.printSchema()
 
 data.show()
+
+# UDF
+import datetime
+def create_date(day, month):
+    month_number = datetime.datetime.strptime(month, "%b").month
+    return datetime.datetime(2020, month_number, day).strftime("%Y-%m-%d")
+
+spark.udf.register("CreateDate", create_date, StringType())
+
+
