2243b103370e192ca8f511692c6c4316c63cb3df
AndyMc629 <andrewpmcmahon629@gmail.com>
2021-08-04 21:16:43 +0100

    Cloned in the mleip-web-service example from the other repo for chapter 5. Will base chapter 7 work on this.
    Also added chapter 7 folder.

---
+++ chapter5/mleip-web-service-main/app/app.py
@@ -0,0 +1,14 @@
+#  Main application file app.py. This web service is a very simple one for returning basic requests that can be built
+# upon later.
+from flask import Flask
+from flask_restful import Api, Resource
+from resources.forecast import ForecastHandler, Forecaster
+
+app = Flask(__name__)
+api = Api(app)
+
+forecaster = Forecaster()
+api.add_resource(ForecastHandler, '/forecast', resource_class_kwargs={'forecaster': forecaster})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000) # change debug=False in production
